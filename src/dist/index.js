1417ff2c0b69ae8386e6dd258c7ad094fc5195a5
kr-sandipprajapati <sandip.prajapati@knackroot.com>
2023-08-02 16:56:10 +0530

    torus provider changes

--- src/dist/index.js
+++ src/dist/index.js
@@ -3,7 +3,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.TorusWallet = void 0;
 const types_1 = require("@web3-react/types");
 const torus_embed_1 = __importDefault(require("@toruslabs/torus-embed"));
 class TorusWallet extends types_1.Connector {
@@ -71,7 +70,7 @@ class TorusWallet extends types_1.Connector {
                     address,
                     symbol,
                     decimals,
-                    image, // A string url of the token logo
+                    image,
                 },
             },
         })
